ff9b79863bc44e6efe23b36daff4a11dfc05bd28
ImranLaruso <36784123+ImranLaruso@users.noreply.github.com>
2018-12-04 10:28:43 +0530

    All User Interfaces. More to be changed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { TeamComponent } from './team/team.component';
+import { EventsComponent } from './events/events.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    TeamComponent,
+    EventsComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    FeedbackComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'events', component: EventsComponent },
+      { path: 'feedback', component: FeedbackComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent },
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
